77af251cca086a026aa427391e3a0e8582125a98
Naveen-kumar-creator <82250995+Naveen-kumar-creator@users.noreply.github.com>
2021-06-26 17:11:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 setup(
     name='seedr',
-    packages=find_packages(include=['copy_method']),
+    packages=find_packages(include=['seedr']),
     version='0.1.0',
     description='Added web preview for links in copy method in pyrogram',
     author='Anonymous',
     license='MIT',
-    install_requires=['pyrogram==1.2.9'],
+    install_requires=['aiohttp[speedups]'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
 )
