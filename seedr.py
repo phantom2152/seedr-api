c6c3f57ce646aa86e914be8600a92f61fbb904b9
Naveen-kumar-creator <82250995+Naveen-kumar-creator@users.noreply.github.com>
2021-06-26 16:41:17 +0530

    Update seedr.py

--- seedr.py
+++ seedr.py
@@ -1,1 +1,11 @@
 import aiohttp
+
+
+seedrApiUrl = "https://www.seedr.cc/oauth_test/resource.php"
+HEROKU_HEADERS = {"Accept": "application/vnd.heroku+json; version=3.cedar-acm", "Content-Type": "application/json"}
+
+class seedr(object):
+    def __int__(self, username, password):
+        self.username = username
+        self.password = password
+        
