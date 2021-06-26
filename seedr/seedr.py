80423cddfbaed20c8ca2659b1a70ab35493ba018
Naveen-kumar-creator <82250995+Naveen-kumar-creator@users.noreply.github.com>
2021-06-26 20:09:51 +0530

    Update seedr.py

--- seedr/seedr.py
+++ seedr/seedr.py
@@ -5,7 +5,7 @@ seedrApiUrl = "https://www.seedr.cc/oauth_test/resource.php"
 
 
 class seedr(object):
-    async def __int__(self, username, password):
+    def __int__(self, username, password):
         self.username = username
         self.password = password
         data = {
