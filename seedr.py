f3474bfc54f81e55b78618a32735915177bd076a
Naveen-kumar-creator <82250995+Naveen-kumar-creator@users.noreply.github.com>
2021-06-26 16:47:11 +0530

    Update seedr.py

--- seedr.py
+++ seedr.py
@@ -2,10 +2,14 @@ import aiohttp
 
 
 seedrApiUrl = "https://www.seedr.cc/oauth_test/resource.php"
-HEROKU_HEADERS = {"Accept": "application/vnd.heroku+json; version=3.cedar-acm", "Content-Type": "application/json"}
+HEROKU_HEADERS = {"Accept": "application/json"; version=3.cedar-acm", "Content-Type": "application/json"}
 
 class seedr(object):
     def __int__(self, username, password):
         self.username = username
         self.password = password
-        
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True) as response:
+                data = await response.json()
+                return data["shortenedUrl"]
+
