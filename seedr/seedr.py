378694c7fea0d084dd917b499a9ec7c7ab958d40
Naveen-kumar-creator <82250995+Naveen-kumar-creator@users.noreply.github.com>
2021-07-02 12:02:01 +0530

    Update seedr.py

--- seedr/seedr.py
+++ seedr/seedr.py
@@ -5,8 +5,9 @@ seedrApiUrl = "https://www.seedr.cc/oauth_test/resource.php"
 
 
 async def postData(url, data):
+    headers = {'User-Agent': 'Mozilla/5.0'}
     async with aiohttp.ClientSession() as session:
-        async with session.post(url, data=data) as response:
+        async with session.post(url, headers=headers, data=data) as response:
             data = await response.json()
             return data
 
@@ -17,9 +18,26 @@ class Seedr:
         self.password = password
     
     async def login(self):
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        payload = {'grant_type': 'password', 'client_id': 'seedr_chrome', 'type': 'login', 'username': self.username, 'password': self.password}
-        async with aiohttp.ClientSession() as session:
-            async with session.post('https://www.seedr.cc/oauth_test/token.php', headers=headers, data=payload) as resp:
-                self.token = (await resp.json())['access_token']
-                return self.token
+        data = {'grant_type': 'password', 'client_id': 'seedr_chrome', 'type': 'login', 'username': self.username, 'password': self.password}
+        response = await postData(url, data)
+        self.token = response['access_token']
+        return self.token
+
+    async addMagnet(self, magnet):
+        data = {'access_token': this.token, 'func': 'add_torrent', 'torrent_magnet': magnet}
+        response = await postData(url, data)
+        return response['data']
+
+    async getVideos():
+        res = [];
+        data = await postData(f"https://www.seedr.cc/api/folder?access_token={this.token}", None)
+        print(data)
+        """for folder in data.data.folders):
+          res.push((await axios("https://www.seedr.cc/api/folder/" + folder.id + "?access_token=" + this.token)).data.files.filter(x => x["play_video"]).map(x => {
+            return {
+              fid: folder.id,
+              id: x["folder_file_id"],
+              name: x.name
+            }"""
+
+    #return res;
