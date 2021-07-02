e2124e92125a7b64bdf734233d7de8a849f49cb5
Naveen-kumar-creator <82250995+Naveen-kumar-creator@users.noreply.github.com>
2021-07-02 12:36:16 +0530

    Update seedr.py

--- seedr/seedr.py
+++ seedr/seedr.py
@@ -9,7 +9,7 @@ class Seedr:
         self.username = username
         self.password = password
 
-    async def postData(url, data):
+    async def postData(self, url, data):
         headers = {'User-Agent': 'Mozilla/5.0'}
         async with aiohttp.ClientSession() as session:
             async with session.post(url, headers=headers, data=data) as response:
@@ -18,19 +18,19 @@ class Seedr:
 
     async def login(self):
         data = {'grant_type': 'password', 'client_id': 'seedr_chrome', 'type': 'login', 'username': self.username, 'password': self.password}
-        response = await postData(seedrApiUrl, data)
+        response = await self.postData(seedrApiUrl, data)
         print(response)
         self.token = response['access_token']
         return self.token
 
     async def addMagnet(self, magnet):
         data = {'access_token': this.token, 'func': 'add_torrent', 'torrent_magnet': magnet}
-        response = await postData(url, data)
+        response = await self.postData(url, data)
         return response['data']
 
     async def getVideos():
         res = [];
-        data = await postData(f"https://www.seedr.cc/api/folder?access_token={this.token}", None)
+        data = await self.postData(f"https://www.seedr.cc/api/folder?access_token={this.token}", None)
         print(data)
         """for folder in data.data.folders):
           res.push((await axios("https://www.seedr.cc/api/folder/" + folder.id + "?access_token=" + this.token)).data.files.filter(x => x["play_video"]).map(x => {
